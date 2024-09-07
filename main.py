0b51fc53e5752b7175cbebf1aa7a96bc6fd65451
Madeleine Woodbury <mail@woodburycode.com>
2024-09-07 18:15:12 -0400

    update root get message

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ app.include_router(clustering.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Clustering Algorithms API is up and running!"}
 
